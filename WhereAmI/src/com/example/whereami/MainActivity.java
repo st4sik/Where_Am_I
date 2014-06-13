33e7ab1dffe441b51c4f98a0fd629c3f5bfe6468
st4sik <ryabokonsm@mail.ru>
2014-06-13 13:25:09 +0400

    Геокодировка

--- WhereAmI/src/com/example/whereami/MainActivity.java
+++ WhereAmI/src/com/example/whereami/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.whereami;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Context;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -30,7 +36,8 @@ public class MainActivity extends Activity {
 		String provider = locationManager.getBestProvider(criteria, true);
 		Location l = locationManager.getLastKnownLocation(provider);
 		updateWithNewLocation(l);
-		locationManager.requestLocationUpdates(provider, 2000, 10, locationListener);
+		locationManager.requestLocationUpdates(provider, 2000, 10,
+				locationListener);
 	}
 
 	private final LocationListener locationListener = new LocationListener() {
@@ -62,13 +69,41 @@ public class MainActivity extends Activity {
 		TextView myLocationText;
 		myLocationText = (TextView) findViewById(R.id.myLocationText);
 		String latLongString = "No location found";
+		String addressString = "No address found";
 		if (location != null) {
 			double lat = location.getLatitude();
 			double lng = location.getLongitude();
 			latLongString = "Lat: " + lat + "\nLong: " + lng;
+
+			double latitude = location.getLatitude();
+			double longitude = location.getLongitude();
+
+			Geocoder gc = new Geocoder(this, Locale.getDefault());
+
+			try {
+
+				List<Address> addresses = gc.getFromLocation(latitude,
+						longitude, 1);
+				StringBuilder sb = new StringBuilder();
+				if (addresses.size() > 0) {
+					Address address = addresses.get(0);
+					for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+						sb.append(address.getAddressLine(i)).append("\n");
+					}
+
+					sb.append(address.getLocality()).append("\n");
+					sb.append(address.getPostalCode()).append("\n");
+					sb.append(address.getCountryName());
+				}
+				addressString = sb.toString();
+
+			} catch (IOException e) {
+
+			}
 		}
 
-		myLocationText.setText("Your Current Position is:\n" + latLongString);
+		myLocationText.setText("Your Current Position is:\n" + latLongString
+				+ "\n\n" + addressString);
 	}
 
 }
